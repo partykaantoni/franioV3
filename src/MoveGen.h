2dec685244d8e6e67231763f4d3c5c0344eca098
MartaPosiadaWszy <93737622+MartaPosiadaWszy@users.noreply.github.com>
2023-02-28 23:57:20 +0100

    Move Gen improvement
    making improvements to the move gen function

--- src/MoveGen.h
+++ src/MoveGen.h
@@ -449,6 +449,23 @@ static inline void generateMoves(moveList* moves) {
 	return;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 static inline void moveGen(moveList* moves) {
 	moves -> count = 0;
 	int originSq, targetSq;
@@ -1339,6 +1356,495 @@ static inline void moveGen2(moveList* moves) {
 						while(attacks) {
 							targetSq = firstBit(attacks);
 							if (getBit(occupancies[OCCUPANCYW], targetSq)) {
+								// Add capture queen
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 1, 0, 0, 0);
+							} else {
+								// Add quiet move queen
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 0, 0, 0, 0);
+							}
+							popWhile(attacks);
+						}
+						popWhile(board);
+					}
+					continue;
+
+				break;
+			}
+		}
+		return;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+static inline void moveGen3(moveList* moves) {
+	moves -> count = 0;
+
+	if (whiteToMove) {
+		int originSq, targetSq;
+		U64 attacks, board;
+		U64 ocupB = occupancies[OCCUPANCYB];
+		U64 ocupW = occupancies[OCCUPANCYW];
+
+		for (int i = WHITE_PAWN; i <= WHITE_KING; i++) {
+			board = pieces[i];
+
+			switch (i) {
+				case WHITE_PAWN:
+			
+					while (board) {
+						originSq = firstBit(board);
+						targetSq = originSq - 8;
+
+						// Pawn Moves
+						if ((targetSq >= a8) && !getBit(occupancies[OCCUPANCY], targetSq)) {
+							if (originSq >= a7 && originSq <= h7) {
+								// Promotion single push
+								moves -> count += 4;
+								moves -> moves[moves -> count - 4] = encodeMove(originSq, targetSq, i, WHITE_QUEEN, 0, 0, 0, 0);
+								moves -> moves[moves -> count - 3] = encodeMove(originSq, targetSq, i, WHITE_ROOK, 0, 0, 0, 0);
+								moves -> moves[moves -> count - 2] = encodeMove(originSq, targetSq, i, WHITE_BISHOP, 0, 0, 0, 0);
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, WHITE_KNIGHT, 0, 0, 0, 0);
+							} else {
+								// Single push
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 0, 0, 0, 0);
+
+
+								if ((originSq >= a2 && originSq <= h2) && (!getBit(occupancies[OCCUPANCY], targetSq - 8))) {
+									// Double push
+									moves -> count++;
+									moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq - 8, i, 0, 0, 1, 0, 0);
+								}
+							}
+						}
+						attacks = WP_ATTACK_MASK[originSq] & ocupB;
+						
+						// Pawn Attacks
+						while (attacks) {
+							targetSq = firstBit(attacks);
+							
+							if (originSq >= a7 && originSq <= h7) {
+								// Promotion Capture
+								moves -> count += 4;
+								moves -> moves[moves -> count - 4] = encodeMove(originSq, targetSq, i, WHITE_QUEEN, 1, 0, 0, 0);
+								moves -> moves[moves -> count - 3] = encodeMove(originSq, targetSq, i, WHITE_ROOK, 1, 0, 0, 0);
+								moves -> moves[moves -> count - 2] = encodeMove(originSq, targetSq, i, WHITE_BISHOP, 1, 0, 0, 0);
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, WHITE_KNIGHT, 1, 0, 0, 0);
+
+							} else {
+								// Capture
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 1, 0, 0, 0);
+							}
+							// popBit(attacks, targetSq);
+							popWhile(attacks);
+						}
+						
+						if (enPassant != -1) {
+							U64 enAttack = BP_ATTACK_MASK[originSq] & (1ULL << enPassant);
+							if (enAttack) {
+								// En Passant Capture
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, firstBit(enAttack), i, 0, 1, 0, 1, 0);
+							}
+						}
+						popWhile(board);
+					}
+					continue;
+
+				break;
+			
+
+
+				case WHITE_KING:
+
+					if (castles & wk) {
+						if(!(CASTLING_MASK[CASTLE_WHITE_KING] & occupancies[OCCUPANCY]) && !isSquareAttacked(f1)) {
+							//addMoveCastle
+							moves -> count++;
+							moves -> moves[moves -> count - 1] = encodeMove(e1, g1, i, 0, 0, 0, 0, 1);
+						}
+					}
+					if (castles & wq) {
+						if(!(CASTLING_MASK[CASTLE_WHITE_QUEEN] & occupancies[OCCUPANCY]) && !isSquareAttacked(d1)) {
+							//addMoveCastle
+							moves -> count++;
+							moves -> moves[moves -> count - 1] = encodeMove(e1, c1, i, 0, 0, 0, 0, 1);
+						}
+					}
+
+					originSq = firstBit(board);
+					attacks = KING_ATTACK_MASK[originSq] & ~ocupW;
+
+					while(attacks) {
+						targetSq = firstBit(attacks);
+						if (getBit(ocupB, targetSq)) {
+							// Add capture king
+							moves -> count++;
+							moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 1, 0, 0, 0);
+						} else {
+							// Add quiet move king
+							moves -> count++;
+							moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 0, 0, 0, 0);
+						}
+						popWhile(attacks);
+					}
+					continue;
+			
+				break;
+			
+			
+			
+				case WHITE_KNIGHT:
+
+					while(board) {
+
+						originSq = firstBit(board);
+						attacks = KNIGHT_ATTACK_MASK[originSq] & ~ocupW;
+
+						while(attacks) {
+
+							targetSq = firstBit(attacks);
+							
+							if (getBit(ocupB, targetSq)) {
+								// Add capture knight
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 1, 0, 0, 0);
+							} else {
+								// Add quiet move knight
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 0, 0, 0, 0);
+							}
+							popWhile(attacks);
+						}
+						popWhile(board);
+					}
+					continue;
+
+				break;
+
+				
+				
+				case WHITE_BISHOP:
+
+					while(board) {
+						originSq = firstBit(board);
+						attacks = getBishopMoves(originSq, occupancies[OCCUPANCY]) & ~ocupW;
+						while(attacks) {
+							targetSq = firstBit(attacks);
+							if (getBit(ocupB, targetSq)) {
+								// Add capture bishop
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 1, 0, 0, 0);
+							} else {
+								// Add quiet move bishop
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 0, 0, 0, 0);
+							}
+							popWhile(attacks);
+						}
+						popWhile(board);
+					}
+					continue;
+
+				break;
+
+
+
+				case WHITE_ROOK:
+
+					while(board) {
+						originSq = firstBit(board);
+						attacks = getRookMoves(originSq, occupancies[OCCUPANCY]) & ~ocupW;
+						while(attacks) {
+							targetSq = firstBit(attacks);
+							if (getBit(ocupB, targetSq)) {
+								// Add capture rook
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 1, 0, 0, 0);
+							} else {
+								// Add quiet move rook
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 0, 0, 0, 0);
+							}
+							popWhile(attacks);
+						}
+						popWhile(board);
+					}
+					continue;
+
+				break;
+
+
+
+				case WHITE_QUEEN:
+
+					while(board) {
+						originSq = firstBit(board);
+						attacks = getQueenMoves(originSq, occupancies[OCCUPANCY]) & ~ocupW;
+						while(attacks) {
+							targetSq = firstBit(attacks);
+							if (getBit(ocupB, targetSq)) {
+								// Add capture queen
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 1, 0, 0, 0);
+							} else {
+								// Add quiet move queen
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 0, 0, 0, 0);
+							}
+							popWhile(attacks);
+						}
+						popWhile(board);
+					}
+					continue;
+
+				break;
+				
+
+
+				default:
+					break;
+			}
+		}
+		return;
+		
+	} else {
+		int originSq, targetSq;
+		U64 attacks, board;
+		U64 ocupB = occupancies[OCCUPANCYB];
+		U64 ocupW = occupancies[OCCUPANCYW];
+
+		for (int i = BLACK_PAWN; i <= BLACK_KING; i++) {
+			board = pieces[i];
+
+			switch (i) {
+				case BLACK_PAWN:
+			
+					while (board) {
+
+						originSq = firstBit(board);
+						targetSq = originSq + 8;
+						
+						// Pawn Moves
+						if ((targetSq <= h1) && !getBit(occupancies[OCCUPANCY], targetSq)) {
+
+							if (originSq >= a2 && originSq <= h2) {
+								moves -> count += 4;
+								moves -> moves[moves -> count - 4] = encodeMove(originSq, targetSq, i, BLACK_QUEEN, 0, 0, 0, 0);
+								moves -> moves[moves -> count - 3] = encodeMove(originSq, targetSq, i, BLACK_ROOK, 0, 0, 0, 0);
+								moves -> moves[moves -> count - 2] = encodeMove(originSq, targetSq, i, BLACK_BISHOP, 0, 0, 0, 0);
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, BLACK_KNIGHT, 0, 0, 0, 0);
+								
+							} else {
+								//addMoveSinglePush
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 0, 0, 0, 0);
+
+								if ((originSq >= a7 && originSq <= h7) && (!getBit(occupancies[OCCUPANCY], targetSq + 8))) {
+									//addMoveDoublePush
+									moves -> count++;
+									moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq + 8, i, 0, 0, 1, 0, 0);
+								}
+							}
+						}
+
+						attacks = BP_ATTACK_MASK[originSq] & ocupW;
+						
+						// Pawn Attacks
+						while (attacks) {
+
+							targetSq = firstBit(attacks);
+							
+							if (originSq >= a2 && originSq <= h2) {
+								//addMovePromotionCapture
+								moves -> count += 4;
+								moves -> moves[moves -> count - 4] = encodeMove(originSq, targetSq, i, BLACK_QUEEN, 1, 0, 0, 0);
+								moves -> moves[moves -> count - 3] = encodeMove(originSq, targetSq, i, BLACK_ROOK, 1, 0, 0, 0);
+								moves -> moves[moves -> count - 2] = encodeMove(originSq, targetSq, i, BLACK_BISHOP, 1, 0, 0, 0);
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, BLACK_KNIGHT, 1, 0, 0, 0);
+							} else {
+								//addMoveCapture
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 1, 0, 0, 0);
+							}
+							popWhile(attacks);
+						}
+						
+						if (enPassant != -1) {
+
+							//u_int enAttack = firstBit(BP_ATTACK_MASK[originSq] & (1ULL << enPassant));
+							U64 enAttack = BP_ATTACK_MASK[originSq] & (1ULL << enPassant);
+							if (enAttack) {
+								//addMoveEnPassantCapture
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, firstBit(enAttack), i, 0, 1, 0, 1, 0);
+							}
+						}
+						popWhile(board);
+					}
+					continue;
+
+				break;
+			
+			
+			
+				case BLACK_KING:
+
+					if (castles & bk) {
+
+						if(!(CASTLING_MASK[CASTLE_BLACK_KING] & occupancies[OCCUPANCY]) && !isSquareAttacked(f8)) {
+							// AddCastlingMove
+							moves -> count++;
+							moves -> moves[moves -> count - 1] = encodeMove(e8, g8, i, 0, 0, 0, 0, 1);
+						}
+					}
+					if (castles & bq) {
+
+						if(!(CASTLING_MASK[CASTLE_BLACK_QUEEN] & occupancies[OCCUPANCY]) && !isSquareAttacked(d8)) {
+							// AddCastlingMove
+							moves -> count++;
+							moves -> moves[moves -> count - 1] = encodeMove(e8, c8, i, 0, 0, 0, 0, 1);
+						}
+					}
+					
+					originSq = firstBit(board);
+					attacks = KING_ATTACK_MASK[originSq] & ~ocupB;
+
+					while(attacks) {
+
+						targetSq = firstBit(attacks);
+
+						if (getBit(ocupW, targetSq)) {
+							// Add capture king
+							moves -> count++;
+							moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 0, 0, 0, 0);
+						} else {
+							// Add quiet move king
+							moves -> count++;
+							moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 1, 0, 0, 0);
+						}
+						popWhile(attacks);
+					}
+					continue;
+
+				break;
+
+				
+				case BLACK_KNIGHT:
+
+					while(board) {
+						originSq = firstBit(board);
+						attacks = KNIGHT_ATTACK_MASK[originSq] & ~ocupB;
+
+						while(attacks) {
+							targetSq = firstBit(attacks);
+							if (getBit(ocupW, targetSq)) {
+								// Add capture knight
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 1, 0, 0, 0);
+							} else {
+								// Add quiet move knight
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 0, 0, 0, 0);
+							}
+							popWhile(attacks);
+						}
+						popWhile(board);
+					}
+					continue;
+
+				break;
+
+
+				case BLACK_BISHOP:
+
+					while(board) {
+
+						originSq = firstBit(board);
+						attacks = getBishopMoves(originSq, occupancies[OCCUPANCY]) & ~ocupB;
+
+						while(attacks) {
+							targetSq = firstBit(attacks);
+							if (getBit(ocupW, targetSq)) {
+								// Add capture bishop
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 1, 0, 0, 0);
+							} else {
+								// Add quiet move bishop
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 0, 0, 0, 0);
+							}
+							popWhile(attacks);
+						}
+						popWhile(board);
+					}
+					continue;
+
+				break;
+
+
+				case BLACK_ROOK:
+
+					while(board) {
+
+						originSq = firstBit(board);
+						attacks = getRookMoves(originSq, occupancies[OCCUPANCY]) & ~ocupB;
+
+						while(attacks) {
+							targetSq = firstBit(attacks);
+							if (getBit(ocupW, targetSq)) {
+								// Add capture rook
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 1, 0, 0, 0);
+							} else {
+								// Add quiet move rook
+								moves -> count++;
+								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 0, 0, 0, 0);
+							}
+							popWhile(attacks);
+						}
+						popWhile(board);
+					}
+					continue;
+
+				break;
+
+
+				case BLACK_QUEEN:
+
+					while(board) {
+
+						originSq = firstBit(board);
+						attacks = getQueenMoves(originSq, occupancies[OCCUPANCY]) & ~ocupB;
+
+						while(attacks) {
+							targetSq = firstBit(attacks);
+							if (getBit(ocupW, targetSq)) {
 								// Add capture queen
 								moves -> count++;
 								moves -> moves[moves -> count - 1] = encodeMove(originSq, targetSq, i, 0, 1, 0, 0, 0);
--- src/franioV3.c
+++ src/franioV3.c
@@ -11,29 +11,37 @@ void debug() {
     //drawPosition();
 
     moveList list[1];
-    int yo = 100000000;
     long start = getTimeMS();
+    int gens[7] =    {100,      1000,        10000,          100000,             1000000,  10000000,     100000000};
+    char* names[7] = {"Hundred", "Thousand", "Ten Thousand", "Hundred Thousand", "Milion", "Ten Milion", "Hundred Milion"};
 
-    for (int i = 0; i < yo; i++) {
-        generateMoves(list);
+    for (int j = 0; j < 7; j++) {
+        printf("\n\nGENERATIONS %d %s\n", gens[j], names[j]);
+        for (int i = 0; i < gens[j]; i++) {
+            generateMoves(list);
+        }
+        printf("\nMove Gen no optimization \ntime %ldms\n", getTimeMS()-start);
+
+        start = getTimeMS();
+        for (int i = 0; i < gens[j]; i++) {
+            moveGen(list);
+        }
+        printf("\nMove Gen switch and splitting loops \ntime %ldms\n", getTimeMS()-start);
+
+        start = getTimeMS();
+        for (int i = 0; i < gens[j]; i++) {
+            moveGen2(list);
+        }
+        printf("\nMove Gen switch and splitting loops \nallocating moves manually \ntime %ldms\n", getTimeMS()-start);
+
+        start = getTimeMS();
+        for (int i = 0; i < gens[j]; i++) {
+            moveGen3(list);
+        }
+        printf("\nMove Gen switch and splitting loops \nallocating moves manually\noccupancies in variables\ntime %ldms\n", getTimeMS()-start);
+        nl();
+
     }
-    printf("\nMove Gen no optimization time %ldms generations : %ld\n\n", getTimeMS()-start, yo);
-    printMoveList(list);
-
-    start = getTimeMS();
-    for (int i = 0; i < yo; i++) {
-        moveGen(list);
-    }
-    printf("\nMove Gen switch and splitting loops time %ldms generations : %ld\n\n", getTimeMS()-start, yo);
-    printMoveList(list);
-
-    start = getTimeMS();
-    for (int i = 0; i < yo; i++) {
-        moveGen2(list);
-    }
-    printf("\nMove Gen switch and splitting loops \nallocating moves manually time %ldms generations : %ld\n\n", getTimeMS()-start, yo);
-    printMoveList(list);
-
 
     return;
 }
